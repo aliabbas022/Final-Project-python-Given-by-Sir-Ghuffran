402f5d9f677045c8296dc51eac907cac40cfb360
aliabbas022 <157229417+aliabbas022@users.noreply.github.com>
2024-03-02 00:07:01 +0500

    guessing.py

--- guessing.py
+++ guessing.py
@@ -1,8 +1,29 @@
+from os import name
 import streamlit as st
 import random
+import tkinter as tk
 
+st.header("Woodloo ")
 st.title("Guessing The Words")
-st.write("A project by Ali Abbas")
+st.write("Welcome to Woodloo")
+def start_game():
+    st.write("Game started!")
+def main():
+    st.title("Test Yourself")
+if st.button("Start Game"):
+        start_game()
 
-user_name = input("Please enter your name: ")
-hello = input("\nhey " +user_name + ", are you ready? " )
+if __name__ == "__main__":
+    main()
+
+
+
+def main():
+    name = st.text_input("Enter your name:")
+
+if name:
+        st.write(f"Hello Champ!")
+
+if __name__ == "__main__":
+    main()
+
