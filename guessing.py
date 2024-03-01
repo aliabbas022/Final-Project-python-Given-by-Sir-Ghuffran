8a36e9a909442e5a6573a682bf05303f93336ca3
aliabbas022 <157229417+aliabbas022@users.noreply.github.com>
2024-03-02 00:40:30 +0500

    guessing.py

--- guessing.py
+++ guessing.py
@@ -6,15 +6,6 @@ import tkinter as tk
 st.header("Woodloo ")
 st.title("Guessing The Words")
 st.write("Welcome to Woodloo")
-def start_game():
-    st.write("Game started!")
-def main():
-    st.title("Test Yourself")
-if st.button("Start Game"):
-        start_game()
-
-if __name__ == "__main__":
-    main()
 
 
 
@@ -23,6 +14,33 @@ def main():
 
 if name:
         st.write(f"Hello Champ!")
+
+if __name__ == "__main__":
+    main()
+
+import streamlit as st
+
+def main():    
+    answer = st.radio("Are You ready?", ("Yes", "No"))
+    if answer == "No":
+        st.write("so why are you here?! hehe")
+    elif answer == "Yes":
+        st.write("start playing!")
+        st.write("You have 3 hints to the secret word, then you'll have to guess it.!")
+
+if __name__ == "__main__":
+    main()
+
+def start_game():
+    st.write("Game started!")
+
+def main():
+    st.title("Test Yourself")
+if st.button("Start Game"):
+        start_game()
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
